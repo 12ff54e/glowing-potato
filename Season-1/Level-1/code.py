18b3461db31204d8f6aa843bfd66130190742641
Q. Zhong <12ff54e@gmail.com>
2024-03-16 02:24:33 +0000

    Finish S1L1

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -16,18 +16,58 @@ from collections import namedtuple
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+def add(xh, xl, yh, yl):
+    rh = xh + yh
+    if abs(xh) > abs(yh):
+        rl = xh - rh + yh + xl + yl
+    else:
+        rl = yh - rh + xh + xl + yl
+
+    zh = rh + rl
+    zl = rh - zh + rl
+
+    return zh, zl
+
+def sub(xh, xl, yh, yl):
+    rh = xh - yh
+    if abs(xh) > abs(yh):
+        rl = xh - rh - yh + xl - yl
+    else:
+        rl = -yh - rh + xh + xl - yl
+
+    zh = rh + rl
+    zl = rh - zh + rl
+
+    return zh, zl
+
 def validorder(order: Order):
-    net = 0
+    net_high = 1e20
+    net_low = 0
+    total_amount = 0
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            # r = net_low + item.amount
+            # rr = net_high
+            # net_high = rr + r
+            # net_low = rr - net_high + r
+            net_high, net_low = add(net_high, net_low, item.amount, 0)
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            # r = net_low - item.amount * item.quantity
+            # rr = net_high
+            # net_high = rr + r
+            # net_low = rr - net_high + r
+            net_high, net_low = sub(net_high, net_low, item.amount * item.quantity, 0)
+            total_amount += item.amount * item.quantity
         else:
             return "Invalid item type: %s" % item.type
 
-    if net != 0:
+
+    if total_amount >= 12*99999:
+        return 'Total amount payable for an order exceeded'
+
+    net = net_high - 1e20 + net_low
+    if abs(net) >= 0.01:
         return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
     else:
         return "Order ID: %s - Full payment received!" % order.id
